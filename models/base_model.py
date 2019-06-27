f1576fa4b750148a7799ae35884c61a3e4239754
ethanpasta <ethan.mayer9@gmail.com>
2019-06-27 20:57:39 +0000

    Basemodule and init file

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,27 @@ from datetime import datetime
 class BaseModel:
     """Base model class"""
 
-    id = uuid4()
-    created_at = datetime.utcnow()
-    updated_at = created_at
+    def __init__(self):
+        """Initialization method for instance"""
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Str method for Base class"""
-        return "[{}] ({}) {}".format("BaseModel", self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute
+        """Updates the public instance attribute
         updated_at with the current datetime"""
-        
-        
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """Method returns a dictionary containing all keys/values
+        of __dict__ instance"""
+        my_dict = self.__dict__
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = datetime.isoformat(self.created_at)
+        my_dict['updated_at'] = datetime.isoformat(self.updated_at)
+        return my_dict
