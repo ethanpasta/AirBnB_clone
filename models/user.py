ff800a06959adcde508e503b054e65d1ce062773
ethanpasta <ethan.mayer9@gmail.com>
2019-06-29 00:30:39 +0000

    Add public attributes with empty string initialization

--- models/user.py
+++ models/user.py
@@ -7,4 +7,11 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class for AirBnB project"""
-    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self):
+        """Initialization method for User class"""
+        BaseModel.__init__(self)
