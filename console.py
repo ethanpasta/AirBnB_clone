a1037a2d6c9bd07f4f2ad439d42342d2cac22390
ethanpasta <ethan.mayer9@gmail.com>
2019-06-29 00:19:05 +0000

    Add tests for line argument before using

--- console.py
+++ console.py
@@ -29,24 +29,25 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, line):
         """Creates an instance of @cls_name class"""
-        args = line.split(" ")
         if not line:
             print("** class name missing **")
-        else:
-            try:
-                obj = eval(args[0])()
-                obj.save()
-                print(obj.id)
-            except NameError:
-                print("** class doesn't exist **")
+            return
+        args = line.split(" ")
+        try:
+            obj = eval(args[0])()
+            obj.save()
+            print(obj.id)
+        except NameError:
+            print("** class doesn't exist **")
 
     def do_show(self, line):
         """Shows an instance of @cls_name class"""
-        args = line.split(" ")
-        l = len(args)
         if not line:
             print("** class name missing **")
-        elif l == 1:
+            return
+        args = line.split(" ")
+        l = len(args)
+        if l != 2:
             print("** instance id missing **")
         else:
             try:
@@ -62,30 +63,31 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """Deletes an instance of class @cls_name"""
-        args = line.split(" ")
         if not line:
             print("** class name missing **")
-        else:
-            try:
-                d = models.storage.objects
-                key = args[0] + '.' + args[1]
-                if key in d:
-                    del d[key]
-                    models.storage.save()
-                else:
-                    print("** no instance found **")
-            except NameError:
-                print("** class doesn't exist **")
+            return
+        args = line.split(" ")
+        try:
+            d = models.storage.objects
+            key = args[0] + '.' + args[1]
+            if key in d:
+                del d[key]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+        except NameError:
+            print("** class doesn't exist **")
 
 
     def do_all(self, line):
         """Shows all instances of @cls_name class"""
-        names = ["BaseModel", "User"]
-        args = line.split(" ")
         d = models.storage.objects
         if not line:
             print([str(x) for x in d.values()])
-        elif args[0] not in names:
+            return
+        names = ["BaseModel", "User"]
+        args = line.split(" ")
+        if args[0] not in names:
             print("** class doesn't exist **")
         else:
             print([str(v) for k, v in d.items() if k.split('.')[0] == args[0]])
@@ -93,11 +95,12 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, line):
         """Updates an instance based on the class name and id by adding or
         updating attribute (save the change into the JSON file)"""
-        args = line.split(" ")
-        l = len(args)
         if not line:
             print("** class name missing **")
-        elif l == 1:
+            return
+        args = line.split(" ")
+        l = len(args)
+        if l == 1:
             print("** instance id missing **")
         elif l == 2:
             print("** attribute name missing **")
