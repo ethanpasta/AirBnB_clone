6133abdce86856c08a1eee95415fe0e06143442d
ethanpasta <ethan.mayer9@gmail.com>
2019-06-29 00:39:53 +0000

    Remove init method

--- models/user.py
+++ models/user.py
@@ -11,7 +11,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self):
-        """Initialization method for User class"""
-        BaseModel.__init__(self)
