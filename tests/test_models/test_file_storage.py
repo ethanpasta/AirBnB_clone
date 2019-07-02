eb33c5de142bcea9c5be5b0304884b85ce91a1bf
Farrukh <679@holbertonschool.com>
2019-07-02 20:02:09 +0000

    Add more tests

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -28,6 +28,7 @@ class TestFileStorage_5(unittest.TestCase):
         Remove file and do additional set up
         """
         file = storage._FileStorage__file_path
+        storage._FileStorage__objects = {}
         if (os.path.exists(file)):
             os.remove(file)
 
@@ -102,8 +103,7 @@ class TestFileStorage_5(unittest.TestCase):
         self.assertFalse(os.path.exists(file))
 
         storage.reload()  # file does not exists
-        # self.assertFalse(storage.all())
-        #self.assertTrue(os.path.exists(file))
+        self.assertFalse(storage.all())
         my_model = BaseModel()
         storage.save()
         self.assertTrue(os.path.exists(file))
@@ -112,3 +112,33 @@ class TestFileStorage_5(unittest.TestCase):
             json_dict = json.load(f)
         temp_dict = {k: v.to_dict() for k, v in storage.all().items()}
         self.assertEqual(temp_dict, json_dict)
+
+    def test_save_basemodel(self):
+        """Checks save of the BaseClass"""
+        file = storage._FileStorage__file_path
+        my_model = BaseModel()
+        updated = my_model.__dict__["updated_at"]
+        obj = storage.all()["{}.{}".format(my_model.__class__.__name__, my_model.id)]
+
+        self.assertEqual(obj.updated_at, updated)
+
+        self.assertFalse(os.path.exists(file))
+        my_model.save()
+
+        self.assertEqual(obj.updated_at, my_model.updated_at)
+        self.assertTrue(os.path.exists(file))
+
+    def test_init_and_new(self):
+        """Tests init and new. If the arguments is dict,
+        then don't call save() of FileStorage"""
+        self.assertEqual(storage.all(), {})
+        d = {"BaseModel.ee49c413-023a-4b49-bd28-f2936c95460d":
+             {"my_number": 89, "__class__": "BaseModel",
+              "updated_at": "2017-09-28T21:07:25.047381",
+              "created_at": "2017-09-28T21:07:25.047372",
+              "name": "Holberton",
+              "id": "ee49c413-023a-4b49-bd28-f2936c95460d"}}
+        BaseModel(**d)
+        self.assertEqual(storage.all(), {})
+        BaseModel()
+        self.assertTrue(storage.all() != {})
